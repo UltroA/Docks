1c50f7b6d0712da2db1beb648a07b75f4492abe5
UltroA <64162002+UltroA@users.noreply.github.com>
2020-08-25 09:12:48 +0300

    Some Docks for kali linux

---
+++ Docks.py
@@ -0,0 +1,16 @@
+print ("Hello %username%, it docs for you.")
+print ("1. DDOS")
+print ("2. Virus --> gif")
+what = input("What do u need?")
+
+if what == "1":
+	print()
+	print ("TO start DDOS go to Website:")
+	print ("https://telegra.ph/DDoS-ataka-na-sajt-pri-pomoshchi-Termux-08-21")
+	end = input()
+	
+if what == "2":
+	print ()
+	print("For Do gif go to Website:")
+	print ("https://telegra.ph/Zarazhenie-PK-s-pomoshchyu-GIF-08-10")
+	end = input()
