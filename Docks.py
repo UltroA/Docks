2da0139489cdc6a393a60dda5f291d4dc0e07d53
UltroA <64162002+UltroA@users.noreply.github.com>
2020-08-26 19:43:40 +0300

    Update Docks.py

--- Docks.py
+++ Docks.py
@@ -1,8 +1,8 @@
 print ("It was downloaded from https://github.com/UltroA/Docks")
 print ("Hello %username%, it docs for you.")
-
 print ("1. DDOS")
 print ("2. Virus --> gif")
+print ("3. Remote Android tool")
 what = input("What do u need?")
 
 if what == "1":
@@ -15,4 +15,9 @@ if what == "2":
 	print ()
 	print("For Do gif go to Website:")
 	print ("https://telegra.ph/Zarazhenie-PK-s-pomoshchyu-GIF-08-10")
+	end = input()
+if what == "3":
+	print ()
+	print("For do ARMT Go to:")
+	print ("https://tgraph.io/Polnyj-dostup-k-chuzhomu-Android-cherez-Termux-08-23-3")
 	end = input()
